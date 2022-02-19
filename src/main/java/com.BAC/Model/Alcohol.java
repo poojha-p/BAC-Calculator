0827d5e49426a03f2586ab6893287d3c2ae651fc
sarsimha <69766312+sarsimha@users.noreply.github.com>
2022-02-19 16:46:13 -0500

    all interfaces

---
+++ src/main/java/com.BAC/Model/Alcohol.java
@@ -0,0 +1,12 @@
+package com.BAC.Model;
+
+public interface Alcohol {
+    String getName();
+    double getOunces();
+    double getVolume();
+
+    void setName(String name);
+    void setOunces(double ounces);
+    void setVolume(double volume);
+
+}
