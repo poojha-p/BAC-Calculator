0b6e28d06278ed94f5f6b9445f6773d023e55638
Poojha Palle <poojhap@ad.unc.edu>
2022-02-20 02:14:06 -0500

    added to js and fixed html

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,2 +1,11 @@
-let drink=prompt("What did you drink today?");
-let weight=prompt("How much do you weigh?")
+let drink=document.getElementById('alc_brand');
+let weight=document.getElementById('weight');
+let amount=document.getElementById('how_much_drunk_oz');
+let gender=document.getElementById('gender');
+
+function bac_calc(drink, weight, amount, gender) {
+    //insert code
+}
+
+// if user has stopped responding to hourly-1.5 hour long wait between updates
+// start messaging emergency contact.
