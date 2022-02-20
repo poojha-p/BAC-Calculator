54a34554cf4373728139a8416d1e25d79ec0c5d4
Sonika Puvvada <69735739+sonikacp@users.noreply.github.com>
2022-02-19 22:08:20 -0500

    view template

---
+++ src/main/java/com.BAC/Main.java
@@ -0,0 +1,11 @@
+package com.BAC;
+
+import javafx.application.Application;
+import com.BAC.View.AppLauncher;
+//import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(AppLauncher.class);
+    }
+}
---
+++ src/main/java/com.BAC/View/FXComponent.java
@@ -0,0 +1,11 @@
+package com.BAC.View;
+
+import javafx.scene.Parent;
+
+//public interface FXComponent {
+//}
+
+public interface FXComponent {
+    /** Renders the component and returns the resulting Parent object */
+    Parent render();
+}
